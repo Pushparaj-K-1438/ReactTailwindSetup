c8dd0f04a1cf14c16182dd6b8662591c644a4826
pushparaj@toolyt <pushparaj@toolyt.com>
2023-11-15 13:34:23 +0530

    React Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+import './globals.css';
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <Routes>
+            {/* Public Routes */}
+            <Route element={<AuthLayout />}>
+              <Route path='/sign-in' element={<SignInForm />}></Route>
+              <Route path='/sign-up' element={<SignUpForm />}></Route>
+            </Route>
+            {/* Private Routes */}
+            <Route element={<RootLayout />}>
+              <Route index element={<Home />}></Route>
+            </Route>
+        </Routes>
+    </main>
+  )
+}
+
+export default App
